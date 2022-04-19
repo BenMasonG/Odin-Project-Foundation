086a065837ba803b655eca7afc46aa13ceedd291
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-19 11:19:44 +0100

    playRound playSelection argument is now case insensitive

--- rockpaperscissors/game-functions.js
+++ rockpaperscissors/game-functions.js
@@ -11,24 +11,28 @@ if (computersTurn === 1) {
 }
 
 
-function playRound (playerSlection, computerSelection) {
-    if (playerSlection === "Rock" && computerSelection === "Scissors") {
+function playRound (playerSelection, computerSelection) {
+    
+   let playerSelection1 = playerSelection[0].toUpperCase() + playerSelection.substring(1).toLowerCase();
+
+    if (playerSelection1 === "Rock" && computerSelection === "Scissors") {
         return "You win Rock beats Scissors."
-    } else if (playerSlection === "Rock" && computerSelection === "Paper") {
+    } else if (playerSelection1 === "Rock" && computerSelection === "Paper") {
         return "You lose Paper beats Rock."
-    } else if (playerSlection === "Rock" && computerSelection === "Rock") {
+    } else if (playerSelection1 === "Rock" && computerSelection === "Rock") {
         return "It's a draw, you both picked Rock."
-    } else if (playerSlection === "Paper" && computerSelection === "Scissors") {
+    } else if (playerSelection1=== "Paper" && computerSelection === "Scissors") {
         return "You lose. Scissors beat Paper."
-    } else if (playerSlection === "Paper" && computerSelection === "Rock") {
+    } else if (playerSelection1 === "Paper" && computerSelection === "Rock") {
         return "You win. Paper beats Rock."
-    } else if (playerSlection === "Paper" && computerSelection === "Paper") {
+    } else if (playerSelection1 === "Paper" && computerSelection === "Paper") {
         return "It's a draw, you both picked Paper."
-    } else if (playerSlection === "Scissors" && computerSelection === "Scissors") {
+    } else if (playerSelection1 === "Scissors" && computerSelection === "Scissors") {
         return "It's a draw. You both picked Scissors."
-    } else if (playerSlection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection1 === "Scissors" && computerSelection === "Paper") {
         return "You win. Scissors beat Paper."
-    } else if (playerSlection === "Scissors" && computerSelection === "Rock") {
+    } else if (playerSelection1 === "Scissors" && computerSelection === "Rock") {
         return "You lose. Rock beats Scissors."
     }   
 }
+

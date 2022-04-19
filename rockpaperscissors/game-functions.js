8f7d135b7b5bfab31bafd438a9576e93ec80bf22
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-04-19 02:15:09 +0100

    playRound function added to game.

--- rockpaperscissors/game-functions.js
+++ rockpaperscissors/game-functions.js
@@ -10,3 +10,25 @@ if (computersTurn === 1) {
 } 
 }
 
+
+function playRound (playerSlection, computerSelection) {
+    if (playerSlection === "Rock" && computerSelection === "Scissors") {
+        return "You win Rock beats Scissors."
+    } else if (playerSlection === "Rock" && computerSelection === "Paper") {
+        return "You lose Paper beats Rock."
+    } else if (playerSlection === "Rock" && computerSelection === "Rock") {
+        return "It's a draw, you both picked Rock."
+    } else if (playerSlection === "Paper" && computerSelection === "Scissors") {
+        return "You lose. Scissors beat Paper."
+    } else if (playerSlection === "Paper" && computerSelection === "Rock") {
+        return "You win. Paper beats Rock."
+    } else if (playerSlection === "Paper" && computerSelection === "Paper") {
+        return "It's a draw, you both picked Paper."
+    } else if (playerSlection === "Scissors" && computerSelection === "Scissors") {
+        return "It's a draw. You both picked Scissors."
+    } else if (playerSlection === "Scissors" && computerSelection === "Paper") {
+        return "You win. Scissors beat Paper."
+    } else if (playerSlection === "Scissors" && computerSelection === "Rock") {
+        return "You lose. Rock beats Scissors."
+    }   
+}
